57c189084cc17daa83ef934babfea07ff74e4478
magdasokolovic <magda.sokolovic@gmail.com>
2021-04-27 09:11:36 +0200

    made the form look nices

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,43 @@
 import React from "react";
 import './Contact.scss'
+import Lottie from 'react-lottie';
+import animationData from './lotties/contact-me.json';
+
+
 
 export default function Contact() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
     return (
         <div className='container'>
             <div className='wrapper'>
                 <p>Feel free to reach me anytime.</p>
                       
+                <div className="flex-container">
+                    <form className="contact-form" method="post" action="">
 
-                <form className="contact-form" method="post" action="">
+                        <input name="name" type="text" className="form-control" placeholder="Your Name" required />
 
-                    <input name="name" type="text" className="form-control" placeholder="Your Name" required />
+                        <input name="email" type="email" className="form-control" placeholder="Your Email" required />
 
-                    <input name="email" type="email" className="form-control" placeholder="Your Email" required />
+                        <textarea name="message" className="form-control" placeholder="Message" rows="4" required></textarea>
 
-                    <textarea name="message" className="form-control" placeholder="Message" rows="4" required></textarea>
+                        <input type="submit" className="form-control submit" value="SEND MESSAGE" />
 
-                    <input type="submit" className="form-control submit" value="SEND MESSAGE" />
+                    </form>
 
-                </form>
+                    <Lottie 
+                        options={defaultOptions}
+                        height={350}
+                        width={500}
+                    />
+                </div>
 
           
             </div>
