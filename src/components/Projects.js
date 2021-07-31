16d6d16fd5c97552ddd640eafe354106d253001d
magdasokolovic <magda.sokolovic@gmail.com>
2021-07-31 09:44:17 +0200

    added webp photos and removed firebase files

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,28 +1,14 @@
 import React from "react";
-import Lottie from 'react-lottie';
-import animationData from './lotties/inProgress.json';
-
 
 export default function Projects() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
+ 
     return (
           <div className='container'>
             <div className='wrapper'>
               <p>
                 my projects:
               </p>
-              <Lottie 
-                options={defaultOptions}
-                height={400}
-                width={400}
-                />
+              
             </div>
           </div>
     );
